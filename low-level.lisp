ac408ed2b99c126c8962ee6adf774b32662aaf7b
Shinmera <shinmera@tymoon.eu>
2017-04-06 11:29:03 +0200

    Export everything.

--- low-level.lisp
+++ low-level.lisp
@@ -1,7 +1,7 @@
 #|
-This file is a part of cl-soloud
-(c) 2017 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
-Author: Nicolas Hafner <shinmera@tymoon.eu>
+ This file is a part of cl-soloud
+ (c) 2017 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
 |#
 
 (in-package #:org.shirakumo.fraf.soloud.cffi)
