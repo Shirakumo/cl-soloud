4d3156fca1b533a3ebc6dc2a2b26e10d5cef16b9
Shinmera <shinmera@tymoon.eu>
2017-04-09 20:10:26 +0200

    Start working on custom filters.

--- filter.lisp
+++ filter.lisp
@@ -101,3 +101,45 @@
 (defmethod set-parameters ((filter bass-boost-filter) &key boost)
   (cl-soloud-cffi:set-bass-boost-filter-params
    (handle filter) boost))
+
+(defvar *filter-object-table* (tg:make-weak-hash-table :test 'eql :weakness :value))
+
+(defclass virtual-filter (filter)
+  ())
+
+(defmethod initialize-instance :after ((filter virtual-filter) &key)
+  (let ((handle (cl-soloud-cffi:create-virtual-filter)))
+    (when (<= handle 0)
+      (error "Failed to create virtual filter."))
+    (setf (handle filter) handle)
+    (tg:finalize filter (lambda () (cl-soloud-cffi:destroy-virtual-filter handle)))
+    (setf (gethash (cffi:pointer-address handle)) filter)))
+
+(defmacro define-filter (name direct-superclasses direct-slots &body options)
+  `(defclass ,name (,@direct-superclasses virtual-filter)
+     ,direct-slots ,@options))
+
+(defgeneric filter (filter samples channels samplerate time))
+(defgeneric filter-channel (filter buffer channel samples channels samplerate time))
+
+(cffi:defcallback filter-filter :void ((instance :pointer) (samples :uint) (channels :uint) (samplerate :float) (time :float))
+  (handler-case
+      (let ((instance (gethash (cffi:pointer-address instance) *filter-object-table*)))
+        (if instance
+            (filter instance samples channels samplerate time)
+            0.0))
+    (error (err)
+      (format T "~&! Error in VirtualFilter::filter callback: ~a~%" err))))
+
+(cl-soloud-cffi:set-virtual-filter-filter (cffi:callback filter-filter))
+
+(cffi:defcallback filter-filter-channel :void ((instance :pointer) (buffer :pointer) (samples :uint) (samplerate :float) (time :float) (channel :uint) (channels :uint))
+  (handler-case
+      (let ((instance (gethash (cffi:pointer-address instance) *filter-object-table*)))
+        (if instance
+            (filter-channel instance buffer channel samples channels samplerate time)
+            0.0))
+    (error (err)
+      (format T "~&! Error in VirtualFilter::filterChannel callback: ~a~%" err))))
+
+(cl-soloud-cffi:set-virtual-filter-filter-channel (cffi:callback filter-filter-channel))
