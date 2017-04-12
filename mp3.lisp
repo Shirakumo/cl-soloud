b595b9fcf8a8b087b62c2efb133ebd6de74347eb
Shinmera <shinmera@tymoon.eu>
2017-04-12 17:59:32 +0200

    First (incomplete, impossible) attempt at an mp3 source in CL.

---
+++ mp3.lisp
@@ -0,0 +1,38 @@
+#|
+ This file is a part of cl-soloud
+ (c) 2017 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package #:org.shirakumo.fraf.soloud)
+
+(defclass mp3-source (virtual-source)
+  ((file :initform NIL :accessor file)))
+
+(defmethod load-file ((source mp3-source) file)
+  (setf (file source) (cl-mpg123:make-file
+                       file
+                       :buffer-size NIL
+                       ;; This is not proper.
+                       :accepted-format (list 44100 :stereo :float))))
+
+(defmethod get-audio ((source mp3-source) buffer samples)
+  (let ((read (cl-mpg123:read-directly (file source) buffer samples)))
+    ;; make sure to zero memory if we reached the end.
+    (loop for i from read below samples
+          do (setf (cffi:mem-aref buffer :float i) 0.0s0))))
+
+(defmethod has-ended ((source mp3-source))
+  (<= (cl-mpg123:sample-count (file source))
+      (cl-mpg123:sample-position (file source))))
+
+(defmethod seek-to ((source mp3-source) time scratch size)
+  ;; I don't actually know what these arguments are.
+  )
+
+(defmethod rewind ((source mp3-source))
+  (cl-mpg123:seek (file source) 0)
+  0)
+
+(defmethod get-info ((source mp3-source) info-key)
+  0.0)
